19dea02ae0a4c8df2b0e53a89768303f1f2e1811
Praveen Jella <praveen.jella@leap360.com>
2024-12-06 20:54:57 +0530

    intial commit

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,77 @@
 #!/bin/bash
 
 TIMESTAMP=$(date +%f-%h-%m-%s)
-
 LOGFILE='/tmp/$TIMESTAMP-$0.log'
+
+VALIDATE(){
+    if [ $1 -ne 0 ] ; then
+        echo "failed $2"
+        exit 1
+    else
+        echo "Success $2"
+    fi
+}
 
 ID=$(id -u)
 
-if [ ID -ne 0 ] ; then
+if [ $ID -ne 0  ] ; then
+    echo "You are not root user"
+    exit 1
+else
+    echo "You are root user"
+fi
+
+dnf module disable nodejs -y
+VALIDATE $? "Disbling old version of nodejs " &>> LOGFILE
+
+dnf module enable nodejs:18 -y
+VALIDATE $? "Enabling of nodejs " &>> LOGFILE
+
+dnf install nodejs -y
+VALIDATE $? "Installation of nodejs " &>> LOGFILE
+
+id roboshop
+
+if [ $? -ne 0 ] ; then
+    useradd roboshop
+    VALIDATE $? "roboshop user added "
+else
+    echo "roboshop user is already there, so skipping adding the user"
+fi
+
+useradd roboshop
+VALIDATE $? "roboshop user adding "
+
+mkdir -p /app
+VALIDATE $? "app directory creation is "
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+VALIDATE $? "Downloading catalogue app"
+
+cd /app
+unzip -o /tmp/catalogue.zip
+VALIDATE $? "Unzipping catalogue is "
+
+npm install
+VALIDATE $? "npm installation is "
+
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $1 "Copying catalogue.service repo is "
+
+systemctl daemon-reload
+VALIDATE $? "daemon reload is "
+
+systemctl enable catalogue
+VALIDATE $? "catalogue enable is "
+
+systemctl start catalogue
+VALIDATE $? "Catalogue starting is "
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-org-shell -y
+VALIDATE $? "Installing MongoDB client is "
+
+mongo --host mongodb.jella.fun </app/schema/catalogue.js
+VALIDATE $? "Loading catalouge data into MongoDB is "
