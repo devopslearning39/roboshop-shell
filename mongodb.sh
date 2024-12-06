1472d8041f65a76487d88d57fbb551c472b27612
Praveen Jella <praveen.jella@leap360.com>
2024-12-06 18:25:19 +0530

    intial commit

--- mongodb.sh
+++ mongodb.sh
@@ -14,8 +14,8 @@ VALIDATE(){
 ID=$(id -u)
 
 if [ ID -ne 0 ] ; then
-    echo "You are not a root user"
-    exit 1
+echo "You are not a root user"
+        exit 1
     else
 echo "You are root user"
 fi
