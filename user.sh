e58323a17382ebd6b0260b64dbf3f45bd3b9c21b
Praveen Jella <praveen.jella@leap360.com>
2024-12-07 12:35:58 +0530

    intial commit

--- user.sh
+++ user.sh
@@ -23,55 +23,55 @@ else
 fi
 
 
-# dnf module disable nodejs -y
-# VALIDATE $? "Disabling existing nodejs"
+dnf module disable nodejs -y
+VALIDATE $? "Disabling existing nodejs"
 
-# dnf module enable nodejs:18 -y
-# VALIDATE $? "Enabling nodejs-18 version"
+dnf module enable nodejs:18 -y
+VALIDATE $? "Enabling nodejs-18 version"
 
-# dnf install nodejs -y
-# VALIDATE $? "Installing node-18 version"
+dnf install nodejs -y
+VALIDATE $? "Installing node-18 version"
 
-# id roboshop
-# if [ $? -ne 0 ] ; then
-#     useradd roboshop
-#     echo "roboshop is user is does not exist...created now."
-# esle
-#     echo "roboshop user is already there, so skipping"
-# fi
+id roboshop
+if [ $? -ne 0 ] ; then
+    useradd roboshop
+    echo "roboshop is user is does not exist...created now."
+esle
+    echo "roboshop user is already there, so skipping"
+fi
 
-# mkdir -p /app
-# VALIDATE $? "Creating app directory"
+mkdir -p /app
+VALIDATE $? "Creating app directory"
 
-# curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip
-# VALIDATE $? "Downloaded application code"
+curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip
+VALIDATE $? "Downloaded application code"
 
-# cd /app 
-# VALIDATE $? "Navigated to app directory"
+cd /app 
+VALIDATE $? "Navigated to app directory"
 
-# unzip -o /tmp/user.zip
-# VALIDATE $? "Unzipping the code"
+unzip -o /tmp/user.zip
+VALIDATE $? "Unzipping the code"
 
-# npm install
-# VALIDATE $? "Installing npm dependencies"
+npm install
+VALIDATE $? "Installing npm dependencies"
 
-# cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
-# VALIDATE $? "Copying user.service file"
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+VALIDATE $? "Copying user.service file"
 
-# systemctl daemon-reload
-# VALIDATE $? "daemon reload"
+systemctl daemon-reload
+VALIDATE $? "daemon reload"
 
-# systemctl enable user 
-# VALIDATE $? "Enabling user"
+systemctl enable user 
+VALIDATE $? "Enabling user"
 
-# systemctl start user
-# VALIDATE $? "Staring user"
+systemctl start user
+VALIDATE $? "Staring user"
 
-# cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
-# VALIDATE $? "Copying mongo repo"
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying mongo repo"
 
-# dnf install mongodb-org-shell -y
-# VALIDATE $? "Installing mongodb org shell"
+dnf install mongodb-org-shell -y
+VALIDATE $? "Installing mongodb org shell"
 
-# mongo --host mongodb.jella.fun </app/schema/user.js
-# VALIDATE $? "Loading user data into mongodb"
+mongo --host mongodb.jella.fun </app/schema/user.js
+VALIDATE $? "Loading user data into mongodb"
