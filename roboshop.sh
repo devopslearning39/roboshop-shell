2d3db90e5fb7cccdaeb0f4cdf04f60467572beb2
Praveen Jella <praveen.jella@leap360.com>
2024-12-09 23:20:27 +0530

    intial commit

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ INSTANCE_TYPE=t2.micro
 SECURITY_GROUP_ID=sg-05014adf54dad41a6
 SUBNET_ID=subnet-074e4b97d8b69706c
 
-aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --tag-specifications 'ResourceType=image,Tags=[{Key='Name',Value='web'}]'
+aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --tag-specifications 'ResourceType=instance,Tags=[{Key='Name',Value='web'}]'
 
 
 if [ $? -ne 0 ] ; then
