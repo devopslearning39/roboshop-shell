160c20c0a18d73444c929e1f3b007a0df9ce4527
Praveen Jella <praveen.jella@leap360.com>
2024-12-09 23:40:33 +0530

    intial commit

--- roboshop.sh
+++ roboshop.sh
@@ -6,8 +6,9 @@ SECURITY_GROUP_ID=sg-05014adf54dad41a6
 SUBNET_ID=subnet-074e4b97d8b69706c
 INSTANCE_NAME=Catalogue
 
-aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --tag-specifications 'ResourceType=instance,Tags=[{Key='Name',Value='$INSTANCE_NAME'}]'
+aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --tag-specifications 'ResourceType=instance,Tags=[{Key='Name',Value='$INSTANCE_NAME'}]' --query 'Reservations[0].Instances[0].NetworkInterfaces[0].PrivateIpAddress' --output text
 
+#  --query 'Instances[0].PrivateIpAddress' --output text)
 
 if [ $? -ne 0 ] ; then
     echo "Something went wrong please check ..!"
