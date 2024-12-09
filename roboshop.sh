6c83c6ade5f0c4c4923276da53757190335d2240
Praveen Jella <praveen.jella@leap360.com>
2024-12-09 23:24:58 +0530

    intial commit

--- roboshop.sh
+++ roboshop.sh
@@ -4,13 +4,13 @@ AMI=ami-0b4f379183e5706b9
 INSTANCE_TYPE=t2.micro
 SECURITY_GROUP_ID=sg-05014adf54dad41a6
 SUBNET_ID=subnet-074e4b97d8b69706c
-INSTANCE_NAME=Catalogue
+INSTANCE_NAME=Checking
 
-aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --tag-specifications 'ResourceType=instance,Tags=[{Key='Name',Value='$INSTANCE_NAME'}]'
+aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --tag-specifications 'ResourceType=instance,Tags=[{Test='Name',Value='$INSTANCE_NAME'}]'
 
 
 if [ $? -ne 0 ] ; then
     echo "Something went wrong please check ..!"
 else
-    echo "$INSTANCE_NAME Created successfully"
+    echo "Created $INSTANCE_NAME instance successfully"
 fi
