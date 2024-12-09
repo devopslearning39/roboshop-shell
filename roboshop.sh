11741fb3574383b8067f8132cfe18e3fe4a4e1f3
Praveen Jella <praveen.jella@leap360.com>
2024-12-09 23:53:26 +0530

    intial commit

--- roboshop.sh
+++ roboshop.sh
@@ -4,9 +4,9 @@ AMI=ami-0b4f379183e5706b9
 INSTANCE_TYPE=t2.micro
 SECURITY_GROUP_ID=sg-05014adf54dad41a6
 SUBNET_ID=subnet-074e4b97d8b69706c
-INSTANCE_NAME=cart
+INSTANCE_NAME=Anil
 
-IP_ADDRESS=(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --tag-specifications 'ResourceType=instance,Tags=[{Key='Name',Value='$INSTANCE_NAME'}]' --query 'Instances[0].PrivateIpAddress' --output text)
+IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --tag-specifications 'ResourceType=instance,Tags=[{Key='Name',Value='$INSTANCE_NAME'}]' --query 'Instances[0].PrivateIpAddress' --output text)
 
 echo "$INSTANCE_NAME PRIVATE IP_ADDRESS: $IP_ADDRESS"
 
