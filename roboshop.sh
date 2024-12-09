cc4d850e2f0d9c5255ef928e5a8551259a5cbcd0
Praveen Jella <praveen.jella@leap360.com>
2024-12-09 22:59:49 +0530

    intial commit

--- roboshop.sh
+++ roboshop.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-IMAGE_ID=i-009c2b75604e28373
+AMI=ami-0b4f379183e5706b9
 INSTANCE_TYPE=t2.micro
 SECURITY_GROUP_ID=sg-05014adf54dad41a6
 SUBNET_ID=subnet-074e4b97d8b69706c
 
-aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID
+aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID
 
-echo "Created new instance"
+if [ $? -ne 0 ] ; then
+    echo "Created new instance successfully"
+else
+    echo "Something went wrong please check ..!"
+fi
