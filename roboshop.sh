1c6219e5fc8b328023b56d32ad3042b80d5801b3
Praveen Jella <praveen.jella@leap360.com>
2024-12-09 23:05:20 +0530

    intial commit

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ INSTANCE_TYPE=t2.micro
 SECURITY_GROUP_ID=sg-05014adf54dad41a6
 SUBNET_ID=subnet-074e4b97d8b69706c
 
-aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID
+aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --tags Key=Jella,Value=Praveen
 
 if [ $? -ne 0 ] ; then
     echo "Something went wrong please check ..!"
