ea9e193ed6d3cd92ec60f5bc75904b632045a550
Praveen Jella <praveen.jella@leap360.com>
2024-12-10 20:17:37 +0530

    intial commit

--- ec2.sh
+++ ec2.sh
@@ -3,9 +3,11 @@
 AMI_ID=ami-0b4f379183e5706b9
 INSTANCE_TYPE=t2.micro
 SECURITY_GROUP_ID=sg-05014adf54dad41a6
+SUBNET_ID=subnet-074e4b97d8b69706c
 INSTANCE_NAME=USER
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id subnet-074e4b97d8b69706c --tags Key=Name,Value=$INSTANCE_NAME
+
+IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --tag-specifications 'ResourceType=instance,Tags=[{Key='Name',Value='$INSTANCE_NAME'}]' --query 'Instances[0].PrivateIpAddress' --output text)
 
 if [ $? -ne 0 ] ; then
     echo "An error occured"
